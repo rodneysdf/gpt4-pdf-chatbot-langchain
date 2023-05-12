76fa00aa3737222f98dab62e505beb692200e517
rodneysdf <rodney.smith@devfactory.com>
2023-05-12 17:12:32 -0600

    start of Amplify

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import '@/styles/base.css';
 import type { AppProps } from 'next/app';
 import { Inter } from 'next/font/google';
+import { Amplify } from "aws-amplify";
+import awsmobile from "../config/aws-exports";
+import "@aws-amplify/ui-react/styles.css";
+
+Amplify.configure({ ...awsmobile, ssr: true });
 
 const inter = Inter({
   variable: '--font-inter',
