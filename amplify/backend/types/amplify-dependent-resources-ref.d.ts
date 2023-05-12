40d0027026c25b6d559b9f2c1fd599b6780e5a0d
rodneysdf <rodney.smith@devfactory.com>
2023-05-12 16:03:22 -0600

    prod environment

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,17 @@
 export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "tpmchatgpt6780338c": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
   "function": {
     "tpmchatgptdfd0678a": {
       "Arn": "string",
