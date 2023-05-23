609a8937937016406683d47b2d30f1347d4f1ce3
rodneysdf <rodney.smith@devfactory.com>
2023-05-23 10:12:40 -0600

    Buildable - changed tsconfig to match node v18

--- amplify/backend/function/tpmchatgptdfd0678a/src/util/google/gdoc.ts
+++ amplify/backend/function/tpmchatgptdfd0678a/src/util/google/gdoc.ts
@@ -68,13 +68,13 @@ export async function readGoogleDoc (documentId: string, credentials: Credential
 
   const title = doc.data.title ?? ''
   console.log(`GDoc '${title}'=`, doc.data)
-  console.log(`GDoc '${title}'=`, doc.data.body)
+  console.log(`GDoc '${title}'=`, JSON.stringify(doc.data.body, null, 4))
 
   const content = convertToString(doc.data.body.content)
   console.log(`GDoc string '${title}'=`, content)
 
-  const docstring = await readStructuralElements(doc.data.body.content)
-  console.log('docstring=', docstring)
+  // const docstring = await readStructuralElements(doc.data.body.content)
+  // console.log('docstring=', docstring)
 
   return {
     title,
@@ -82,51 +82,51 @@ export async function readGoogleDoc (documentId: string, credentials: Credential
   }
 }
 
-async function readParagraphElement (element: docs_v1.Schema$ParagraphElement): Promise<string> {
-  const run = element.textRun
-  if (run === null || run?.content === null) {
-    // The TextRun can be null if there is an inline object.
-    return ''
-  }
-  return run?.content || ''
-}
+// async function readParagraphElement (element: docs_v1.Schema$ParagraphElement): Promise<string> {
+//   const run = element.textRun
+//   if (run === null || run?.content === null) {
+//     // The TextRun can be null if there is an inline object.
+//     return ''
+//   }
+//   return run?.content || ''
+// }
 
-async function readStructuralElements (elements: docs_v1.Schema$StructuralElement[]): Promise<string> {
-  let text = ''
-  for (const element of elements) {
-    if (element.paragraph !== null) {
-      for (const paragraphElement of ((element?.paragraph?.elements) != null) || []) {
-        text += await readParagraphElement(paragraphElement)
-      }
-    } else if (element.table !== null) {
-      // The text in table cells are in nested Structural Elements and tables may be
-      // nested.
-      for (const row of ((element?.table?.tableRows) != null) || []) {
-        for (const cell of (row.tableCells != null) || []) {
-          text += await readStructuralElements((cell.content != null) || [])
-        }
-      }
-    } else if (element.tableOfContents !== null) {
-      // The text in the TOC is also in a Structural Element.
-      text += await readStructuralElements(((element?.tableOfContents?.content) != null) || [])
-    }
-  }
-  return text
-}
+// async function readStructuralElements (elements: docs_v1.Schema$StructuralElement[]): Promise<string> {
+//   let text = ''
+//   for (const element of elements) {
+//     if (element.paragraph !== null) {
+//       for (const paragraphElement of ((element?.paragraph?.elements) != null) || []) {
+//         text += await readParagraphElement(paragraphElement)
+//       }
+//     } else if (element.table !== null) {
+//       // The text in table cells are in nested Structural Elements and tables may be
+//       // nested.
+//       for (const row of ((element?.table?.tableRows) != null) || []) {
+//         for (const cell of (row.tableCells != null) || []) {
+//           text += await readStructuralElements((cell.content != null) || [])
+//         }
+//       }
+//     } else if (element.tableOfContents !== null) {
+//       // The text in the TOC is also in a Structural Element.
+//       text += await readStructuralElements(((element?.tableOfContents?.content) != null) || [])
+//     }
+//   }
+//   return text
+// }
 
-async function extractTextFromDocument (document: docs_v1.Schema$Document): Promise<string> {
-  let text = ''
-  const elements = document?.body?.content
-  if (elements != null) {
-    elements.forEach((contentElement) => {
-      if (contentElement.paragraph != null) {
-        contentElement.paragraph.elements.forEach((paragraphElement) => {
-          if (paragraphElement.textRun != null) {
-            text += paragraphElement.textRun.content
-          }
-        })
-      }
-    })
-  }
-  return text
-}
+// async function extractTextFromDocument (document: docs_v1.Schema$Document): Promise<string> {
+//   let text = ''
+//   const elements = document?.body?.content
+//   if (elements != null) {
+//     elements.forEach((contentElement) => {
+//       if (contentElement.paragraph != null) {
+//         contentElement.paragraph.elements.forEach((paragraphElement) => {
+//           if (paragraphElement.textRun != null) {
+//             text += paragraphElement.textRun.content
+//           }
+//         })
+//       }
+//     })
+//   }
+//   return text
+// }
